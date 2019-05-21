c788e4922d6ebb888f8cc2d9cd43659a12b21cad
hugbubby <hugbubby@protonmail.com>
2019-05-21 12:45:08 -0500

    More work.

---
+++ Chapter2/chapter2.py
@@ -0,0 +1,30 @@
+from plotting import plot
+
+
+
+#Quiz 2.4.4
+def addn(v, w):
+    return [v[i] + w[i] for i in range(len(v))]
+
+#Quiz 2.5.3
+def scalar_vector_mult(alpha, v):
+    return [alpha * v[i] for i in range(len(v))]
+
+
+
+
+#Task 2.3.1
+L = [[2,2], [3,2], [1.75,1], [2,1], [2.25,1], [2.5,1], [2.75,1], [3,1], [3.25,1]]
+def face_by_vectors():
+    plot(L,4)
+
+#Task 2.4.3
+def face_translated():
+    plot([addn(v, [1,2]) for v in L], 4)
+
+#Task 2.5.4
+def face_scaled():
+    plot([scalar_vector_mult(0.5,l) for l in L], 4)
+    plot([scalar_vector_mult(-0.5,l) for l in L], 4)
+
+face_scaled()
